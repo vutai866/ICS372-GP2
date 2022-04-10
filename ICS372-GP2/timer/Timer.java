9bc4eec070e50d1f024eecdbcef2ccc44094362a
Tai Vu <vutai866@gmail.com>
2022-04-10 13:38:46 -0500

    Update Timer.java

--- ICS372-GP2/timer/Timer.java
+++ ICS372-GP2/timer/Timer.java
@@ -1,5 +1,53 @@
 package timer;
 
-public class Timer {
+// Import library
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
+public class Timer implements PropertyChangeListener{
+    private int timeValue;
+    private Notifiable client;
+
+    public Timer(Notifiable client, int timeValue){
+        this.client = client;
+        this.timeValue = timeValue;
+        Clock.getInstance().addPropertyChangeListener(this);
+    }// end Timer
+
+    /**
+     * Change the time value by adding 
+     * @param value
+     */
+    public void addTimeValue(int value){
+        timeValue += value;
+    }// end addTimeValue
+
+    /**
+     * Stop the timer by deleting itself from the list of observers
+     * @param
+     */
+    public void stop(){
+        Clock.getInstance().removeProtertyChangeListener(this);
+    }// end stop
+
+    /**
+     * Get the time value of the timer
+     * @param timeValue
+     */
+    public int getTimeValue(){
+        return timeValue;
+    }// end getTimeValue
+
+    /**
+     * 
+    */
+    @Override
+    public void propertyChange(PropertyChangeEvent arg0){
+        if(--timeValue <= 0){
+            client.onTimerRunsOut();
+            Clock.getInstance().removePropertyChangeListener(this);
+        } else{
+            client.OnTimerTick(timeValue);  
+        }// end else
+    }
 }
