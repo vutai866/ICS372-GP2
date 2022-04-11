58a162b58b15cd461bdd3f866f87f5abe734e625
Tai Vu <vutai866@gmail.com>
2022-04-11 11:52:16 -0500

    Update Notifiable.java

--- ICS372-GP2/timer/Notifiable.java
+++ ICS372-GP2/timer/Notifiable.java
@@ -2,4 +2,14 @@ package timer;
 
 public class Notifiable {
 
-}
+    /**
+     * Process timer ticks
+     */
+    public void OnTimerTick(int timerValue);
+
+    /**
+     * Process timer runs out event
+     */
+    public void OnTimerRunsOut();
+
+}// end Notifiable
