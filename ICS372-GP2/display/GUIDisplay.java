cf2b327899aa5fa8eca9a8946cc194a678ba22bf
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2022-04-18 16:44:20 -0500

    added to GUIDisplay
    trainStatecontext not done so GUIDisplay not done too
    more details can be added to TrainDisplay
    
    Not sure how to connect GUIDisplay and main
    so that it runs

--- ICS372-GP2/display/GUIDisplay.java
+++ ICS372-GP2/display/GUIDisplay.java
@@ -1,5 +1,99 @@
 package display;
 
-public class GUIDisplay {
+import buttons.DoorObstructingButton;
+import buttons.GUIButton;
+import buttons.StationReachedButton;
+import buttons.StationReachingButton;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GUIDisplay extends Application implements TrainDisplay {
+	private GUIButton doorObstructing;
+	private GUIButton stationReached;
+	private GUIButton stationReaching;
+	private Text trainStatus;
+	private Text doorStatus;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		doorObstructing = new DoorObstructingButton("door obstructing");
+		stationReached = new StationReachedButton("station reached");
+		stationReaching = new StationReachingButton("station reaching");
+		GridPane pane = new GridPane();
+		pane.setHgap(10);
+		pane.setVgap(10);
+		pane.setPadding(new Insets(10, 10, 10, 10));
+		pane.add(trainStatus, 0, 0);
+		pane.add(doorStatus, 1, 0);
+		pane.add(stationReaching, 2, 0);
+		pane.add(stationReached, 3, 0);
+		pane.add(doorObstructing, 4, 0);
+
+		Scene scene = new Scene(pane);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Microwave Version 2");
+		// TrainContext.getInstance().setDisplay(this);
+		// not yet done
+		primaryStage.show();
+
+	}
 //test
+
+	@Override
+	public void showTrainStopped(int time) {
+		// not yet done
+
+	}
+
+	@Override
+	public void showTrainAccelerating() {
+		trainStatus.setText("train accelerating");
+
+	}
+
+	@Override
+	public void showTrainDecelerating() {
+		trainStatus.setText("train decelerating");
+
+	}
+
+	@Override
+	public void showTrainFullSpeed() {
+		trainStatus.setText("train going full speed");
+
+	}
+
+	@Override
+	public void showDoorOpened() {
+		doorStatus.setText("door opened");
+
+	}
+
+	@Override
+	public void showDoorOpening() {
+		doorStatus.setText("door opening");
+
+	}
+
+	@Override
+	public void showDoorClosed() {
+		doorStatus.setText("door closed");
+
+	}
+
+	@Override
+	public void showDoorCLosing() {
+		doorStatus.setText("door closing");
+
+	}
+
+	@Override
+	public void showDoorObstruction() {
+		doorStatus.setText("door obstructed");
+
+	}
 }
--- ICS372-GP2/display/TrainDisplay.java
+++ ICS372-GP2/display/TrainDisplay.java
@@ -1,5 +1,51 @@
 package display;
 
-public class TrainDisplay {
+public interface TrainDisplay {
+	/**
+	 * Displays the cook time remaining
+	 * 
+	 * @param time remaining time
+	 */
+	public void showTrainStopped(int time);
+
+	/**
+	 * Indicate that the train is speeding up
+	 */
+	public void showTrainAccelerating();
+
+	/**
+	 * Indicate that the train is slowing down
+	 */
+	public void showTrainDecelerating();
+
+	/**
+	 * Indicate that train is going full speed
+	 */
+	public void showTrainFullSpeed();
+
+	/**
+	 * Indicate that the door is now open
+	 */
+	public void showDoorOpened();
+
+	/**
+	 * indicate that door is opening
+	 */
+	public void showDoorOpening();
+
+	/**
+	 * indicate that door is closed
+	 */
+	public void showDoorClosed();
+
+	/**
+	 * indicate that door is closing
+	 */
+	public void showDoorCLosing();
+
+	/**
+	 * indicate that door is obstructed
+	 */
+	public void showDoorObstruction();
 
 }
