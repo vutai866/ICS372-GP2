250f7d60ab236b5fff34b90f3cf39491912b88b4
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2022-04-23 23:37:09 -0500

    fixing so main show the Gui when pressed

--- ICS372-GP2/module-info.java
+++ ICS372-GP2/module-info.java
@@ -1,6 +1,10 @@
 module ICS372 {
 	requires javafx.controls;
+	requires javafx.base;
 	requires java.desktop;
 
 	opens start to javafx.graphics, javafx.fxml;
+
+	exports display to javafx.graphics;
+	exports start to javafx.graphics;
 }
--- ICS372-GP2/start/Main.java
+++ ICS372-GP2/start/Main.java
@@ -1,25 +1,18 @@
 package start;
 
+import display.GUIDisplay;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import timer.Clock;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+public class Main {
 
 	public static void main(String[] args) {
-		launch(args);
+		Clock.getInstance();
+		new Thread() {
+			@Override
+			public void run() {
+				Application.launch(GUIDisplay.class, null);
+			}
+		}.start();
 	}
 }
