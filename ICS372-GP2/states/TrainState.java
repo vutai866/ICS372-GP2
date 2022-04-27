c367a575339cc640c8934236917f5c7ddc09fb0e
madisonlinke <90859673+madisonlinke@users.noreply.github.com>
2022-04-26 20:15:37 -0500

    updated trainState

--- ICS372-GP2/states/TrainState.java
+++ ICS372-GP2/states/TrainState.java
@@ -1,11 +1,44 @@
 package states;
 
 public abstract class TrainState {
+	
 	public abstract void enter();
+	
 	public abstract void exit();
 	
 	public void onTimerTick(int timerValue) {
 		
 	}
 	
+	public void onAccelerate() {
+		
+	}
+	
+	public void onDeccelerate() {
+		
+	}
+	
+	public void onDoorsOpened() {
+		
+	}
+	
+	public void onDoorsClosed() {
+		
+	}
+	
+	public void onDoorsClosing() {
+		
+	}
+	
+	public void onDoorsOpening() {
+		
+	}
+	
+	public void onFullSpeed() {
+		
+	}
+	
+	public void onStopped() {
+		
+	}
 }
